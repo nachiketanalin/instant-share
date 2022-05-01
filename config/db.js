1b55291dd9c9d85aaaee6eed532b5233363b2c1e
Nachiketa Nalin <nalinnachiketa@gmail.com>
2022-05-02 00:53:19 +0530

    initial commit

---
+++ config/db.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+function connectDB(){
+
+    mongoose.connect(process.env.MONGO_CONNECTION_URL , { useNewUrlParser: true, useUnifiedTopology: true});
+    // , useCreateIndex:true , useFindAndModify : true 
+    const connection = mongoose.connection;
+
+    connection.once('open', () => {
+        console.log('Database connected');
+    }).on('error', function (err) {
+        console.log(err);
+      });
+}
+
+module.exports = connectDB;
