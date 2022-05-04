5168af09f035e0980d37505574692586f1581325
Nachiketa Nalin <nalinnachiketa@gmail.com>
2022-05-04 19:08:50 +0530

    updates

--- routes/files.js
+++ routes/files.js
@@ -49,38 +49,8 @@ router.post('/send', async (req, res) => {
         return res.status(422).send({ error: 'All fields are required except expiry.'});
     }
     // Get data from db 
-    // try {
-    //     const file = await File.findOne({ uuid: uuid });
-    //     if(file.sender) {
-    //         return res.status(422).send({ error: 'Email already sent once.'});
-    //     }
-    //     file.sender = emailFrom;
-    //     file.receiver = emailTo;
-    //     const response = await file.save();
-
-    //     // send mail
-    //     const sendMail = require('../services/emailService');
-    //     sendMail({
-    //         from: emailFrom,
-    //         to: emailTo,
-    //         subject: 'instantShare file sharing',
-    //         text: `${emailFrom} shared a file with you.`,
-    //         html: require('../services/emailTemplate')({
-    //                 emailFrom, 
-    //                 downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}?source=email`,
-    //                 size: parseInt(file.size/1000) + ' KB',
-    //                 expires: '24 hours'
-    //             })
-    //     }).then(() => {
-    //         return res.json({success: true});
-    //     }).catch(err => {
-    //         return res.status(500).json({error: 'Error in email sending.'});
-    //     });
-    // } catch(err) {
-    // return res.status(500).send({ error: 'Something went wrong.'});
-    // }
-
-    const file = await File.findOne({ uuid: uuid });
+    try {
+        const file = await File.findOne({ uuid: uuid });
         if(file.sender) {
             return res.status(422).send({ error: 'Email already sent once.'});
         }
@@ -101,8 +71,38 @@ router.post('/send', async (req, res) => {
                     size: parseInt(file.size/1000) + ' KB',
                     expires: '24 hours'
                 })
+        }).then(() => {
+            return res.json({success: true});
+        }).catch(err => {
+            return res.status(500).json({error: 'Error in email sending.'});
         });
-        return res.send({success: true});
+    } catch(err) {
+    return res.status(500).send({ error: 'Something went wrong.'});
+    }
+
+    // const file = await File.findOne({ uuid: uuid });
+    //     if(file.sender) {
+    //         return res.status(422).send({ error: 'Email already sent once.'});
+    //     }
+    //     file.sender = emailFrom;
+    //     file.receiver = emailTo;
+    //     const response = await file.save();
+
+    //     // send mail
+    //     const sendMail = require('../services/emailService');
+    //     sendMail({
+    //         from: emailFrom,
+    //         to: emailTo,
+    //         subject: 'instantShare file sharing',
+    //         text: `${emailFrom} shared a file with you.`,
+    //         html: require('../services/emailTemplate')({
+    //                 emailFrom, 
+    //                 downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}?source=email`,
+    //                 size: parseInt(file.size/1000) + ' KB',
+    //                 expires: '24 hours'
+    //             })
+    //     });
+    //     return res.send({success: true});
                 
 
 });
